bd5ba107c9bca4200fe5d9f5213e3d3335799499
KirillSSS <savchuk.kirill.ua@gmail.com>
2021-03-04 17:40:31 +0200

    final

--- Terraform/database.tf
+++ Terraform/database.tf
@@ -8,6 +8,7 @@ resource "aws_db_instance" "aws-database" {
   password             = "q1q1q1q1"
   db_subnet_group_name = aws_db_subnet_group.aws-db-subnet.id
   vpc_security_group_ids = [aws_security_group.aws-sg-database.id]
+  publicly_accessible = true
   skip_final_snapshot  = true
   identifier           = "aws-database"
 }
